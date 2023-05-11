e0f7eda656c201c06893e759a3d8e1f681ad6f5d
GBI <83820869+GT5679@users.noreply.github.com>
2023-05-11 06:36:51 +0200

    [FEATURE] Extend version.in with fw and hw info

--- sources/OpenWizeUp_Options.cmake
+++ sources/OpenWizeUp_Options.cmake
@@ -1,14 +1,27 @@
+
+string(TIMESTAMP TODAY "%Y-%m-%d %H:%M:%S")
+
 ################################################################################
 function(display_option)
     message ("      -> USE_PHY_TRIG                    : ${USE_PHY_TRIG}")
     message ("      -> USE_PHY_LAYER_TRACE             : ${USE_PHY_LAYER_TRACE}")
     message ("      -> HAS_HIRES_TIME_MEAS             : ${HAS_HIRES_TIME_MEAS}")
+    
     message ("      -> HAS_WIZE_CORE_EXTEND_PARAMETER  : ${HAS_WIZE_CORE_EXTEND_PARAMETER}")
     message ("      -> HAS_LOW_POWER_PARAMETER         : ${HAS_LOW_POWER_PARAMETER}")
     message ("      -> HAS_TEST_MODE_PARAMETER         : ${HAS_TEST_MODE_PARAMETER}")
     message ("      -> HAS_EXTEND_PARAMETER            : ${HAS_EXTEND_PARAMETER}")
+    
     message ("      -> BUILD_STANDALAONE_APP           : ${BUILD_STANDALAONE_APP}")
     message ("      -> BUILD_NVM_BINARY                : ${BUILD_NVM_BINARY}")
+
+    message ("      -> HW_NAME         : ${HW_NAME}")
+    message ("      -> HW_VENDOR       : ${HW_VENDOR}")
+    message ("      -> HW_MODEL        : ${HW_MODEL}")
+    message ("      -> HW_VER_MAJ      : ${HW_VER_MAJ}")
+    message ("      -> HW_VER_MIN      : ${HW_VER_MIN}")
+    message ("      -> HW_VER_REV      : ${HW_VER_REV}")
+    message ("      -> HW_DATE         : ${HW_DATE}")
 endfunction(display_option)
 
 ################################################################################
@@ -21,17 +34,26 @@ set(PARAM_XML_FILE_LIST "")
 set(PARAM_XML_FILE_LIST "${PARAM_XML_FILE_LIST} ${DEFAULT_CFG_FILE_DIR}/LANParams.xml")
 
 #-------------------------------------------------------------------------------
-option(USE_PHY_TRIG                      "TODO : description." OFF)
-option(USE_PHY_LAYER_TRACE               "TODO : description." ON)
-option(HAS_HIRES_TIME_MEAS               "TODO : description." ON)
-option(HAS_WIZE_CORE_EXTEND_PARAMETER    "TODO : description." ON)
-option(HAS_LOW_POWER_PARAMETER           "TODO : description." ON)
+option(USE_PHY_TRIG                      "Use the PHY trigger pin as TX/RX command" OFF)
+option(USE_PHY_LAYER_TRACE               "Enable the PHY layer trace messages." ON)
+option(HAS_HIRES_TIME_MEAS               "Define if High-Resolution timer is present (used to get the clock on PONG message)." ON)
+option(HAS_WIZE_CORE_EXTEND_PARAMETER    "Use the low power xml file." ON)
+option(HAS_LOW_POWER_PARAMETER           "Use the low power xml file." ON)
 
-option(HAS_TEST_MODE_PARAMETER           "TODO : description." ON)
-option(HAS_EXTEND_PARAMETER              "TODO : description." ON)
+option(HAS_TEST_MODE_PARAMETER           "Use the test mode xml file." ON)
+option(HAS_EXTEND_PARAMETER              "Use the extended parameter xml file." ON)
 
-option(BUILD_STANDALAONE_APP             "TODO : description." OFF)
-option(BUILD_NVM_BINARY                  "TODO : description." OFF)
+option(BUILD_STANDALAONE_APP             "Use this option when the bootstrap is not used." OFF)
+option(BUILD_NVM_BINARY                  "Build the non-volatile memory area content and produce a binary and elf files." OFF)
+
+# HW info
+set(HW_NAME    "WIZEUP" CACHE STRING "Define the board name print when ATI command is called.")
+set(HW_VENDOR  "ALCIOM" CACHE STRING "Define the board vendor print when ATI command is called.")
+set(HW_MODEL   "WZ1000" CACHE STRING "Define the board model print when ATI command is called.")
+set(HW_VER_MAJ 0 CACHE STRING "Define the board major version number.")
+set(HW_VER_MIN 1 CACHE STRING "Define the board minor version number.")
+set(HW_VER_REV 0 CACHE STRING "Define the board revision version number.")
+set(HW_DATE ${TODAY} CACHE STRING "Define the board date.")
 
 #-------------------------------------------------------------------------------
 if(USE_PHY_TRIG)
