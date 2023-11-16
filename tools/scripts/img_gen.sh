8590ea4542d0ed3ce6d8d1b38bbb052bcc7f9e5d
GBI <83820869+GT5679@users.noreply.github.com>
2023-11-16 15:51:50 +0100

    [FIX] Fix padding at end of img

--- tools/scripts/img_gen.sh
+++ tools/scripts/img_gen.sh
@@ -16,7 +16,7 @@ make install
 ***          Build the application + nvm            ***
 mkdir -p _build
 cd _build
-cmake -DCMAKE_BUILD_TYPE=Debug -DGENERATE_PARAM=ON -DBUILD_NVM_BINARY=ON ../.
+cmake -DCMAKE_BUILD_TYPE=Debug -DGENERATE_PARAM=ON -DBUILD_NVM_BINARY=ON -DBUILD_STANDALONE_APP=OFF ../.
 make -j4
 make install
 
@@ -62,10 +62,14 @@ declare -A mLocal=(
     [app_size]=x
     [img_file]=x
     [img_size]=x
+    [padding_size]=0
     [header_size]=512
-    [le_hex_sz]=x
-    [le_magic]=x
-    [le_magic_dead]=x    
+    [hex_img_size]=x
+    [hex_magic]=x
+    [hex_magic_dead]=x
+    [le_hex_img_size]=x
+    [le_hex_magic]=x
+    [le_hex_magic_dead]=x
     #---
 );
 
@@ -120,35 +124,90 @@ function get_app_info
         exit 0;
     fi
 
-    # get file size
-    app_size=$(stat -c %s ${mLocal[app_file]});
-    mLocal[app_size]=${app_size};
+    # Get file size
+    mLocal[app_size]=$(stat -c %s ${mLocal[app_file]});
+   
+    # Get magic head and foot
+    local img_magic=_magic_I${img_id}_;
+    mLocal[hex_magic]=${info[${img_magic}]}
+    mLocal[hex_magic_dead]=${info[_magic_dead_]}
+    
+    mLocal[le_hex_magic]=$( echo ${mLocal[hex_magic]} | rev | dd conv=swab 2>/dev/null);
+    mLocal[le_hex_magic_dead]=$( echo ${mLocal[hex_magic_dead]} | rev | dd conv=swab 2>/dev/null);
+
+    # ----------
+    app_size=${mLocal[app_size]};
     
     # add header size (512 bytes)
     app_size=$(( ${app_size} + ${mLocal[header_size]} )); 
     
     # add magic dead size (4 bytes)
     app_size=$(( ${app_size} + 4 )); 
-    mLocal[img_size]=${app_size};
     
-    # convert in LE
-    hex_sz=$(printf "%08x\n" "${app_size}");
-    le_hex_sz=$( echo ${hex_sz} | rev | dd conv=swab 2>/dev/null);
-    mLocal[le_hex_sz]=${le_hex_sz};
+    # Compute padding to be 8 bytes aligned
+    nb_padding=0
+    need_padding=$(( ${app_size} % 8));
+    if [[ ${need_padding} != 0 ]]
+    then
+        nb_padding=$(( 8 - ${need_padding} ));
+    fi
+    app_size=$(( ${app_size} + ${nb_padding} ));
+
+    # ----------
+    # Set Image output filename
+    mLocal[img_file]=$(dirname ${mLocal[app_file]})/"img_${img_id}.bin";
+    # Set Image padding
+    mLocal[padding_size]=${nb_padding};
+    # Set Image size
+    mLocal[img_size]=$(( ${app_size} ));
+    # Set Image size hex
+    mLocal[hex_img_size]=$(printf "%08x\n" "${mLocal[img_size]}");   
+    # Set Image size hex LE
+    mLocal[le_hex_img_size]=$( echo ${mLocal[hex_img_size]} | rev | dd conv=swab 2>/dev/null);
+}
+
+# ---------------------------------------------------
+function check_local_info
+{
+    local res="";
+    local ckecking_ok=1;
+    local format_str="   -> %-15s: %s ";
     
+    echo "-> Checking";
     
-    # create header
-    local img_magic=_magic_I${img_id}_;
-    le_magic=$( echo ${info[${img_magic}]} | rev | dd conv=swab 2>/dev/null);
-    mLocal[le_magic]=${le_magic};
+    # -------------------------------
+    res="OK";
+    # Check 8 bytes alignement
+    is_modulo=$(( ${mLocal[img_size]} % 8));
+    #is_modulo=5;
+    if [[ ${is_modulo} != 0 ]]
+    then
+        res="Bad";
+        ckecking_ok=0;
+    fi
+    printf "${format_str}(%d mod 8 = %d)\n" "Alignment" ${res} ${mLocal[img_size]} ${is_modulo};
     
-    le_magic_dead=$( echo ${info[_magic_dead_]} | rev | dd conv=swab 2>/dev/null);
-    mLocal[le_magic_dead]=${le_magic_dead};
-            
-    # get in/out directory
-    local app_dir=$(dirname ${mLocal[app_file]});
-    # set output filename
-    mLocal[img_file]=${app_dir}/"img_${img_id}.bin";
+    # -------------------------------
+    res="OK";    
+    # Check size not exceed max allocated size
+    img_max_size=$( printf "%d" 0x${info[_I${img_id}_size_]} );
+    #img_max_size=100000;
+    exceed_size=$(( ${mLocal[img_size]} - ${img_max_size} ));
+    #echo " exceed_size ${exceed_size}";
+    if [[ ${exceed_size} -gt 0 ]]
+    then
+        res="Bad";
+        ckecking_ok=0;
+    fi
+    printf "${format_str}(%d - % d) = %d\n" "Img size" ${res} ${mLocal[img_size]} ${img_max_size} ${exceed_size};
+
+     # -------------------------------
+    if [[ ${ckecking_ok} != 1 ]]
+    then
+        echo "  *--- Error ---*";
+    else
+        echo "  *---- OK ----*";
+    fi
 }
 
 # ---------------------------------------------------
@@ -160,20 +219,19 @@ function img_bin_gen
     echo "-> Write in file ${img_file}";
     # ---
     local seek=0;
-    echo "   -> Add magic ${mLocal[le_magic]}";
-    echo ${mLocal[le_magic]} | xxd -r -p | dd of=${img_file} obs=4 count=1 conv=notrunc 2>/dev/null ;
+    echo "   -> Add magic 0x${mLocal[le_hex_magic]}";
+    echo ${mLocal[le_hex_magic]} | xxd -r -p | dd of=${img_file} obs=4 count=1 conv=notrunc 2>/dev/null ;
 
     # ---
     seek=$(( ${seek} + 1 ));
-    echo "   -> Add size ${mLocal[le_hex_sz]}";
-    echo ${mLocal[le_hex_sz]} | xxd -r -p | dd of=${img_file} obs=4 count=1 seek=${seek} conv=notrunc 2>/dev/null ;
+    echo "   -> Add size 0x${mLocal[le_hex_img_size]}";
+    echo ${mLocal[le_hex_img_size]} | xxd -r -p | dd of=${img_file} obs=4 count=1 seek=${seek} conv=notrunc 2>/dev/null ;
 
 
-    # ---
+    # --- Padding for Header
     padding=FFFFFFFF;
-    #nb_padding=$(( 32 / 4 - 2))
     nb_padding=$(( ${mLocal[header_size]} / 4 - 2))
-    echo "   -> Add padding ${nb_padding} x ${padding} ";
+    echo "   -> Add padding ${nb_padding} x 0x${padding} ";
     for (( c=0; c<${nb_padding}; c++ ))
     do 
         seek=$(( ${seek} + 1 ));
@@ -185,11 +243,22 @@ function img_bin_gen
     seek=$(( ${seek} + 1 ));
     echo "   -> Add bin ${fName}";
     dd if=${mLocal[app_file]} of=${img_file} obs=4 seek=${seek} 2>/dev/null ;
-
-    # write magic dead
+    
+    # write magic dead   
     seek=$(( ${mLocal[img_size]} - 4  )); 
-    echo "   -> Add magic dead ${mLocal[le_magic_dead]}";
-    echo ${mLocal[le_magic_dead]} | xxd -r -p | dd of=${img_file} obs=1 count=1 seek=${seek} conv=notrunc 2>/dev/null ;
+    echo "   -> Add magic dead ${mLocal[le_hex_magic_dead]}";
+    echo ${mLocal[le_hex_magic_dead]} | xxd -r -p | dd of=${img_file} obs=1 count=1 seek=${seek} conv=notrunc 2>/dev/null ;
+
+    # Add padding
+    padding=aa;
+    nb_padding=${mLocal[padding_size]};
+    seek=$(( ${seek} - ${nb_padding} ));
+    echo "   -> Add padding ${nb_padding} x 0x${padding} ";
+    for (( c=0; c<${nb_padding}; c++ ))
+    do 
+        echo ${padding} | xxd -r -p | dd of=${img_file} obs=1 count=1 seek=${seek} conv=notrunc 2>/dev/null ;
+        seek=$(( ${seek} + 1 ));
+    done;   
 }
 
 #*******************************************************************************
@@ -217,13 +286,15 @@ function show_me_input
     done
     
     echo "************************";
-    echo "-> App file : ${mLocal[app_file]}";
-    echo "-> App size : ${mLocal[app_size]}";
-    echo "-> Header size : ${mLocal[header_size]}";
-    echo "-> Magic word (LE) : 0x ${mLocal[le_magic]}";
-    echo "-> Magic dead (LE) : 0x ${mLocal[le_magic_dead]}";    
-    echo "-> Image file (out) : ${mLocal[img_file]}";
-    echo "-> Image size : ${mLocal[img_size]} (0x ${mLocal[le_hex_sz]})";
+    printf " -> App file         : %s \n" ${mLocal[app_file]};
+    printf " -> App size         : %d (0x%08x) \n" ${mLocal[app_size]} ${mLocal[app_size]};
+    printf " -> Header size      : %d \n" ${mLocal[header_size]};
+    printf " -> Magic word       : 0x%08x (LE 0x%08x) \n" 0x${mLocal[hex_magic]} 0x${mLocal[le_hex_magic]};
+    printf " -> Magic dead       : 0x%08x (LE 0x%08x) \n" 0x${mLocal[hex_magic_dead]} 0x${mLocal[le_hex_magic_dead]};
+    printf " -> Image file (out) : %s \n" ${mLocal[img_file]};
+    printf " -> Image size       : %d (0x%08x) (LE 0x%08x) \n" ${mLocal[img_size]} 0x${mLocal[hex_img_size]} 0x${mLocal[le_hex_img_size]};
+    printf " -> Image padding    : %d \n" ${mLocal[padding_size]};
+    check_local_info;
     echo "************************";
 }
 
