931d013c50bae06cac7ba190dd386e916a25c770
GBI <83820869+GT5679@users.noreply.github.com>
2023-11-22 11:26:23 +0100

    [FIX] Ability to chose the app bin file name

--- tools/scripts/img_gen.sh
+++ tools/scripts/img_gen.sh
@@ -1,43 +1,6 @@
 #!/bin/bash 
 
 #*******************************************************************************
-function help_me
-{
-cat << EOF
-#######################################################
-***             Build the bootstrap                 ***
-mkdir -p _bootstrap_build
-cd _bootstrap_build
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_CFG=Bootstrap ../. 
-make -j4
-make install
-
-#######################################################
-***          Build the application + nvm            ***
-mkdir -p _build
-cd _build
-cmake -DCMAKE_BUILD_TYPE=Debug -DGENERATE_PARAM=ON -DBUILD_NVM_BINARY=ON -DBUILD_STANDALONE_APP=OFF ../.
-make -j4
-make install
-
-#######################################################
-***            Generate Img 0 or 1                  ***
-From "OpenWize-Up" directory
-
-# TODO : It will be integrate in the git repo ....
-# export PATH=\$PATH:\$(pwd)/tools/scripts
-# ...but for now just call the local script
-
-To generate img_0.bin
-./img_gen.sh ./_install
-
-To generate img_1.bin
-./img_gen.sh ./_install 1
-
-EOF
-}
-
-#*******************************************************************************
 declare -A info=(
     #---   
     [bootstrap_file]=x
@@ -49,12 +12,8 @@ declare -A info=(
     [_magic_I0_]=x
     [_magic_I1_]=x
     [_magic_dead_]=deadc0de
-    #[_magic_dead_]=DEADC0DE
-    #[__end_prog]=x
     #---
 );
-search="_B_org_ _I0_org_ _I0_size_ _I1_org_ _I1_size_ _magic_I0_ _magic_I1_";
-img_id=0;
 
 declare -A mLocal=(
     #---   
@@ -74,6 +33,9 @@ declare -A mLocal=(
 );
 
 # ---------------------------------------------------
+#
+# Get info from bootstrap.elf
+#
 function get_part_address 
 {
     local cmd="arm-none-eabi-nm ${info[bootstrap_file]}";
@@ -108,6 +70,9 @@ function get_part_address
 }
 
 # ---------------------------------------------------
+#
+# Compute application (img) info
+#
 function get_app_info
 {
     # check variable
@@ -167,50 +132,9 @@ function get_app_info
 }
 
 # ---------------------------------------------------
-function check_local_info
-{
-    local res="";
-    local ckecking_ok=1;
-    local format_str="   -> %-15s: %s ";
-    
-    echo "-> Checking";
-    
-    # -------------------------------
-    res="OK";
-    # Check 8 bytes alignement
-    is_modulo=$(( ${mLocal[img_size]} % 8));
-    #is_modulo=5;
-    if [[ ${is_modulo} != 0 ]]
-    then
-        res="Bad";
-        ckecking_ok=0;
-    fi
-    printf "${format_str}(%d mod 8 = %d)\n" "Alignment" ${res} ${mLocal[img_size]} ${is_modulo};
-    
-    # -------------------------------
-    res="OK";    
-    # Check size not exceed max allocated size
-    img_max_size=$( printf "%d" 0x${info[_I${img_id}_size_]} );
-    #img_max_size=100000;
-    exceed_size=$(( ${mLocal[img_size]} - ${img_max_size} ));
-    #echo " exceed_size ${exceed_size}";
-    if [[ ${exceed_size} -gt 0 ]]
-    then
-        res="Bad";
-        ckecking_ok=0;
-    fi
-    printf "${format_str}(%d - % d) = %d\n" "Img size" ${res} ${mLocal[img_size]} ${img_max_size} ${exceed_size};
-
-     # -------------------------------
-    if [[ ${ckecking_ok} != 1 ]]
-    then
-        echo "  *--- Error ---*";
-    else
-        echo "  *---- OK ----*";
-    fi
-}
-
-# ---------------------------------------------------
+#
+# Generate image file
+#
 function img_bin_gen
 {
     local img_file=${mLocal[img_file]};
@@ -261,8 +185,118 @@ function img_bin_gen
     done;   
 }
 
+# ---------------------------------------------------
+#
+# Check application (img) info
+#
+function check_local_info
+{
+    local res="";
+    local ckecking_ok=1;
+    local format_str="   -> %-15s: %s ";
+    local format_2_str="    | %-15s: %s ";
+    
+    echo "-> Checking";
+    
+    # -------------------------------
+    res="OK";
+    # Check 8 bytes alignement
+    is_modulo=$(( ${mLocal[img_size]} % 8));
+    #is_modulo=5;
+    if [[ ${is_modulo} != 0 ]]
+    then
+        res="Bad";
+        ckecking_ok=0;
+    fi
+    printf "${format_str}(%d mod 8 = %d)\n" "Alignment" ${res} ${mLocal[img_size]} ${is_modulo};
+    
+    # -------------------------------
+    res="OK";
+    # Check 210 bytes alignement of FW binary
+    is_modulo=$(( ${mLocal[app_size]} % 210));
+    if [[ ${is_modulo} != 0 ]]
+    then
+        res="Bad";
+        ckecking_ok=0;       
+        printf "${format_2_str}(%d mod 210 = %d)\n" "..." ${res} ${mLocal[app_size]} ${is_modulo};
+        
+        # Downloadable image
+        padding=$(( 210 - ${is_modulo} ));
+        new_size=$(( ${mLocal[app_size]} + ${padding} ));
+        printf "${format_2_str}(New size = %d; Padding = %d)\n" "..." "" ${new_size} ${padding};        
+        
+        # Get the allocated downloadable image size
+        down_max_size=$( printf "%d" 0x${info[_I${img_id}_size_]} );
+        down_max_size=$(( ${down_max_size} - ${mLocal[header_size]} ));
+        is_modulo=$(( ${down_max_size} % 210));
+        if [[ ${is_modulo} != 0 ]]
+        then
+            down_max_size=$(( ${down_max_size} - ${is_modulo} ));
+        fi
+        
+        res="OK";
+        # Check size not exceed max downloadable allocated size
+        exceed_size=$(( ${new_size} - ${down_max_size} ));
+        if [[ ${exceed_size} -gt 0 ]]
+        then
+            res="Bad";
+            ckecking_ok=0;
+        fi
+        printf "${format_2_str}(%d  - %d) = %d\n" "..." ${res} ${new_size} ${down_max_size} ${exceed_size};
+        
+    fi
+    #printf "${format_str}(%d mod 210 = %d)\n" "Alignment" ${res} ${mLocal[app_size]} ${is_modulo};
+    #printf "${format_str}(%d with %d padding)\n" "New size" "OK" ${new_size} ${padding};
+    
+    # -------------------------------
+    res="OK";
+    # Check size not exceed max allocated size
+    img_max_size=$( printf "%d" 0x${info[_I${img_id}_size_]} );
+    #img_max_size=100000;
+    exceed_size=$(( ${mLocal[img_size]} - ${img_max_size} ));
+    #echo " exceed_size ${exceed_size}";
+    if [[ ${exceed_size} -gt 0 ]]
+    then
+        res="Bad";
+        ckecking_ok=0;
+    fi
+    printf "${format_str}(%d - % d) = %d\n" "Img size" ${res} ${mLocal[img_size]} ${img_max_size} ${exceed_size};
+
+     # -------------------------------
+    if [[ ${ckecking_ok} != 1 ]]
+    then
+        echo "  *--- Error ---*";
+    else
+        echo "  *---- OK ----*";
+    fi
+}
+
+# ---------------------------------------------------
+#
+# Get NVM info
+#
+function get_nvm_address
+{
+    local elf_fname=${install_dir}/${bin_file_name%.*}.elf;
+    # check if file exist
+    if [[ -f ${elf_fname} ]]
+    then
+        local cmd="arm-none-eabi-nm ${elf_fname}";
+        local res=($($cmd | grep "__nvm_org__"));
+        if [[ "${res[0]}" != "" ]]
+        then 
+            nvm_address="at address 0x${res[0]}";
+        fi
+    fi
+}
+
+#*******************************************************************************
 #*******************************************************************************
 
+# ---------------------------------------------------
+#
+# Show info from bootstrap.elf
+#
 function show_me_input
 {
     echo "************************";
@@ -299,6 +333,9 @@ function show_me_input
 }
 
 # ---------------------------------------------------
+#
+# Show part of generate image
+#
 function show_me_output
 {
     local img_file=${mLocal[img_file]};
@@ -324,46 +361,114 @@ function show_me_output
 }
 
 # ---------------------------------------------------
+#
+# Usage
+#
+function usage
+{
+cat << EOF
+
+Usage: $0 [ -d inst_dir ] [ -b bin_file ] [ -i img_id] [-p] [-h]
+ -d inst_dir : Directory where to find bootstrap.elf and app bin file (default : ${install_dir})
+ -b bin_file : The application binary file name (default : ${bin_file_name})
+ -i img_id   : The image id to generate (default : ${img_id})
+ -p : show a kind of "How To"
+ -h : this help
+EOF
+}
+
+#*******************************************************************************
+#*******************************************************************************
+
+# ---------------------------------------------------
 function show_me_next
 {
 local img_address=_I${img_id}_org_;
 cat << EOF
 1. Mass Erase Flash 
 2. Load "${mLocal[img_file]}" at address 0x${info[${img_address}]}
-3. Load nvm_area_strip file
+3. Load "${install_dir}/nvm_area_strip.bin" ${nvm_address} 
 4. Load "${info[bootstrap_file]}" (or .bin file) at address 0x${info[_B_org_]}
 EOF
 }
 
+# ---------------------------------------------------
+function show_me_process
+{
+cat << EOF
+#######################################################
+***             Build the bootstrap                 ***
+mkdir -p _bootstrap_build
+cd _bootstrap_build
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_CFG=Bootstrap ../. 
+make -j4
+make install
+
+#######################################################
+***          Build the application + nvm            ***
+mkdir -p _build
+cd _build
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_NVM_BINARY=ON ../.
+make -j4
+make install
+
+#######################################################
+***            Generate Img 0 or 1                  ***
+From "OpenWize-Up" directory
+
+To generate img_0.bin
+tools/scripts/img_gen.sh -i 0
+
+To generate img_1.bin
+tools/scripts/img_gen.sh -i 1
+
+EOF
+}
+
 #*******************************************************************************
-if [[ $1 == "" ]]
-then
-    help_me;
-    exit 0;
-fi
+#*******************************************************************************
 
-# Get dir where bootstrap elf and app bin files are installed
-install_dir=$1;
-if [[ ! -d ${install_dir} ]]
-then
-    echo "Directory \"${install_dir}\" doesn't exist!!!";
-    exit 0;
-fi
+install_dir="./_install/bin";
+bin_file_name="App_WizeUp.bin";
+img_id=0;
 
-# Get part id (if any, otherwise the default is part 0)
-if [[ $2 != "" ]]
-then
-    if [[ $2 == 1 ]]
-    then
-        img_id=1;
-    else
-        img_id=0;
-    fi
-fi
+while getopts "d:b:i:hp" options
+do
+    case "${options}" in
+        d)
+            # Get dir where bootstrap elf and app bin files are installed
+            install_dir=${OPTARG};
+            ;;
+        b)
+            # Get binary file name
+            bin_file_name=${OPTARG};
+            ;;
+        i)
+            # Get part id
+            if [[ ${OPTARG} -gt 1 ]]
+            then
+                echo "Error. The image id must be 0 or 1";
+                usage;
+                exit;
+            fi
+            img_id=${OPTARG};
+            ;;
+        p)
+            show_me_process;
+            exit;
+            ;;
+        *)
+            usage;
+            exit;
+            ;;        
+    esac
+done;
 
 # ---------------------------------------------------
+nvm_address="";
+
 info[bootstrap_file]="${install_dir}/bootstrap.elf";
-mLocal[app_file]="${install_dir}/App_WizeUp.bin";
+mLocal[app_file]="${install_dir}/${bin_file_name}";
 
 get_part_address;
 get_app_info;
@@ -372,6 +477,7 @@ show_me_input;
 img_bin_gen;
 show_me_output;
 
+get_nvm_address;
 show_me_next;
 
 # ---------------------------------------------------
